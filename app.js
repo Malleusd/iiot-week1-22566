ca50f272491ff40f8a07d0c552a923faa3434556
Malleusd <malleusdraconia63@gmail.com>
2023-10-20 15:28:32 +0700

    first app

---
+++ app.js
@@ -0,0 +1,9 @@
+let banana = 5;
+
+{ 
+ const banana = 10
+ console.log(banana - 1);
+}
+
+banana = banana - 1;
+console.log(banana);
---
+++ lamp.js
@@ -0,0 +1,19 @@
+//function switch_on () {
+//    document.getElementById('lamp').src = "light_on.png";
+//    
+//}
+
+//function switch_off () {
+//    document.getElementById('lamp').src = "light_off.png";
+//    
+//}
+
+function light(show) {
+    var lamp;
+    if (show == 0) {
+    lamp = "light_off.png";
+} else {
+    lamp = "light_on.png";
+}
+document.getElementById("iaamp").src = lamp
+}
